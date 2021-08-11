77c7e5bc6fe824c5f054e49fa80b5be036005629
Nathan Henniges <demonwolf@demonwolfdev.com>
2021-08-10 19:46:43 -0500

    Switched to Go.

--- discord/index.js
+++
@@ -1,7 +0,0 @@
-const Discord = require('discord.js');
-
-const discord = new Discord.Client({
-  autoReconnect: true,
-});
-
-module.exports = discord;
---
+++ pkg/twitch/twitch.go
@@ -0,0 +1,9 @@
+// package twitch
+
+// import (
+// 	twitchgo "github.com/gempir/go-twitch-irc/v2"
+// )
+
+// func sayPing(c *twitchgo.Client) {
+// 	c.Say("mrdemonwolf", "Hello World")
+// }
